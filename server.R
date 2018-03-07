574e9c4bb17f6c5a8cc7c72ffd4c96b92a66cd2a
Lisa Qing <lisaqing98@gmail.com>
2018-03-06 16:54:14 -0800

    Finished visualizations for the most part

--- server.R
+++ server.R
@@ -7,45 +7,24 @@ source("ReadingKagg.R")
 df <- getFile()
 
 my.server <- function(input, output){
-  output$scatter <- renderPlot({
+
+  output$scatter <- renderPlotly({
     data <- select(df, Country, Age, seek_help, care_options) %>% 
       filter(Age < 100, Age > 0) ## Filter out outliers such as 99999 years old
     data$Country[data$Country == "Bahamas, The"] = "Bahamas"
+    
     data$seek_help[data$seek_help == "Don't know"] = "No" 
-    #data <- group_by(data, Country, seek_help, care_options) %>%
-    #  summarize(average_age = mean(Age), count = n())
-    #data <- filter(data, Country == input$place)
-    
-    
-    #ggplot(data = data) +
-    #  geom_point(mapping = aes(x = average_age, y = care_options, color = seek_help, size = count)) 
-    
+
     data <- group_by(data, Country, seek_help) %>%
       summarize(average_age = mean(Age), count = n())
-    #data <- filter(data, Country == input$place)
     
+    plot_ly(data = data, x = ~average_age, y = ~Country, color = ~seek_help, size = ~count, colors = "Set1", type = 'scatter',
+            text = ~paste(Country, paste0("Average Age: ", round(average_age, digits = 1), 
+                                          paste0("\nNumber of respondents: ", count)), sep = "<br />"), hoverinfo = "text") %>%
+      layout(title = 'World Wide View of Age and Seeking Mental Health Help Trend \n(hover for more information)\n',
+             xaxis = list(title = 'Average Age'),
+             yaxis = list(title = 'Country'), margin=list(l = 100), yaxis=list(tickprefix="\t"))
     
-    ggplot(data = data) +
-      geom_point(mapping = aes(x = average_age, y = Country, color = seek_help, size = count)) +
-      theme(
-        panel.background = element_rect(fill = "white",
-                                        colour = "white",
-                                        size = 0.5, linetype = "dotted"),
-        panel.grid.major = element_line(size = 0.25, linetype = 'solid',
-                                        colour = "gray"),
-        panel.grid.minor = element_line(size = 0.25, linetype = 'dotted',
-                                        colour = "lightgray")
-      )
-    
-    
-    # world <- map_data("world")
-    # world <- mutate(world, Country.Code = iso.alpha(world$region, n = 3))
-    # data <- mutate(data, Country.Code = iso.alpha(data$Country, n = 3))
-    # data <- left_join(data, world)
-    
-    # ggplot(data = data) +
-    #  geom_polygon(aes(x = long, y = lat, group = group, fill = Age)) +
-    #  coord_quickmap()
   })
   
   output$bar <- renderPlotly({
@@ -57,7 +36,7 @@ my.server <- function(input, output){
       summarize(count = n())
     care_options <- c("Not Sure", "No", "Yes")
     survey.data.2 <- data.frame(care_options)
-    # values = survey.data.1$count
+
     value1 = survey.data.1$count[survey.data.1$seek_help == "No"]
     value2 = survey.data.1$count[survey.data.1$seek_help == "Don't know"]
     value3 = survey.data.1$count[survey.data.1$seek_help == "Yes"]
@@ -65,15 +44,12 @@ my.server <- function(input, output){
     survey.data.2$Not_sure <- value2
     survey.data.2$Yes_help <- value3
     
-    # data$seek_help[data$seek_help == "Don't know"] = "No" 
-
-    #ggplot(data = survey.data, aes(x = care_options, fill = seek_help)) + 
-    #  geom_bar() + scale_fill_brewer(palette = "Diverging") 
-    
     plot_ly(survey.data.2, x = ~care_options, y = ~No_help, type = 'bar', name = 'No help seeked') %>%
       add_trace(y = ~Not_sure, name = 'Not sure') %>%
       add_trace(y = ~Yes_help, name = 'Help seeked') %>%
-      layout(yaxis = list(title = 'Count'), barmode = 'group')
+      layout(yaxis = list(title = 'Count'), xaxis = list(title = 'Company Offered Care Options'),
+             title = 'World Wide View of Seeking Mental Health Help and Offered Care Options', 
+             barmode = 'group') 
     
     
   })
@@ -98,24 +74,17 @@ my.server <- function(input, output){
       summarize(count = n())
   })
   
-  output$scatter_spec <- renderPlot({
+  output$scatter_spec <- renderPlotly({
       data1 <- filter_data()
-      
-      ggplot(data = data1) +
-        geom_point(mapping = aes(x = Age, y = care_options, color = seek_help, size = count)) + 
-        theme(
-          panel.background = element_rect(fill = "white",
-                                          colour = "white",
-                                          size = 0.5, linetype = "dotted"),
-          panel.grid.major = element_line(size = 0.25, linetype = 'solid',
-                                          colour = "gray"),
-          panel.grid.minor = element_line(size = 0.25, linetype = 'dotted',
-                                          colour = "lightgray")
-        )
-      
+    
+      plot_ly(data = data1, x = ~Age, y = ~care_options, color = ~seek_help, size = ~count, colors = "Set1", type = 'scatter', 
+              text = ~paste(paste0("Age: ", Age), paste0("\nNumber of respondents: ", count), paste0("\nHelp Seeked: ", seek_help), 
+                            sep = "<br />"), hoverinfo = "text") %>%
+        layout(title = paste('Age v. Offered Care Options \n(w/ influence of company offered care options)\n'),
+               yaxis = list(title = 'Company Offered Care Options'), margin=list(l = 100), yaxis=list(tickprefix="\t"))
   })
   
-  output$us_scatter <- renderPlot({
+  output$us_scatter <- renderPlotly({
     data <- select(df, Country, state, Age, seek_help, care_options) %>% 
       filter(Age < 100, Age > 0, Country == "United States") 
       ## Filter out outliers such as 99999 years old
@@ -125,18 +94,14 @@ my.server <- function(input, output){
       summarize(average_age = mean(Age), count = n())
     #data <- filter(data, Country == input$place)
     
-    
-    ggplot(data = data) +
-      geom_point(mapping = aes(x = average_age, y = state, color = seek_help, size = count)) +
-      theme(
-        panel.background = element_rect(fill = "white",
-                                        colour = "white",
-                                        size = 0.5, linetype = "dotted"),
-        panel.grid.major = element_line(size = 0.25, linetype = 'solid',
-                                        colour = "gray"),
-        panel.grid.minor = element_line(size = 0.25, linetype = 'dotted',
-                                        colour = "lightgray")
-      )
+    plot_ly(data = data, x = ~average_age, y = ~state, color = ~seek_help, size = ~count, colors = "Set1", type = 'scatter', 
+            text = ~paste(state, paste0("Average Age: ", round(average_age, digits = 1), 
+                                          paste0("\nNumber of respondents: ", count)), sep = "<br />"), hoverinfo = "text") %>%
+      layout(title = 'United States Detailed View of Age and Seeking Mental Health Help Trend \n(hover for more information) \n',
+             xaxis = list(title = 'Average Age'),
+             yaxis = list(title = 'State (abbreviated)')) 
+      
+
   })
   
   output$us_bar <- renderPlotly({
@@ -155,16 +120,12 @@ my.server <- function(input, output){
     survey.data.2$No_help <- value1
     survey.data.2$Not_sure <- value2
     survey.data.2$Yes_help <- value3
-    
-    # data$seek_help[data$seek_help == "Don't know"] = "No" 
-    
-    #ggplot(data = survey.data, aes(x = care_options, fill = seek_help)) + 
-    #  geom_bar() + scale_fill_brewer(palette = "Diverging") 
-    
+
     plot_ly(survey.data.2, x = ~care_options, y = ~No_help, type = 'bar', name = 'No help seeked') %>%
       add_trace(y = ~Not_sure, name = 'Not sure') %>%
       add_trace(y = ~Yes_help, name = 'Help seeked') %>%
-      layout(yaxis = list(title = 'Count'), barmode = 'group')
-    
+      layout(title = 'United States Detailed View of Seeking Mental Health Help and Offered Care Options Trend',
+             xaxis = list(title = 'Company Offered Care Options'),
+             yaxis = list(title = 'Count'), width = 800, height = 500)
   })
 }
--- ui.R
+++ ui.R
@@ -1,29 +1,40 @@
 library("shiny")
 library("plotly")
+# library(DT)
 
 my.ui <- fluidPage(
-    mainPanel(
-    p("display"),
-    tabsetPanel(tabPanel("World Wide Info", plotOutput("scatter"), br(),
-                         p("The above scatterplot shows the average age of respondents in each
-                           country as well as whether the respondents have seeked help in the past.
-                           In the survey, individuals sometimes put 'Not sure' and that was 
-                           interpretted as no help seeked."), br(),
-                         plotlyOutput("bar"), br(), p("The bar graph shows in detail the TOTAL number of 
-                                                respondents who have not seeked help and have. For a 
-                                                better breakdown the respondents who said 'Not sure' are
-                                                shown as well."), br(),
-                         tableOutput("table"), br(), p("The table shows a breakdown on where all the data came
-                                                 from.")),
-                tabPanel("Countries", fluidRow(selectInput("place", "Country of Interest", 
-                              c("Australia", "Austria", "Bahamas", "Belgium", "Bosnia and Herzegovina",
-                                "Brazil", "Bulgaria", "Canada", "China", "Colombia", "Costa Rica", "Croatia",
-                                "Czech Republic", "Denmark", "Finland", "France", "Georgia", "Germany",
-                                "Greece", "Hungary", "India", "Ireland", "Israel", "Italy", "Japan", "Latvia",
-                                "Mexico", "Moldova", "Netherlands", "New Zealand", "Nigeria", "Norway",
-                                "Philippines", "Poland", "Portugal", "Romania", "Russia", "Singapore", "Slovenia",
-                                "South Africa", "Spain", "Sweden", "Switzerland", "Thailand", "United Kingdom",
-                                "United States", "Uruguay")), plotOutput("scatter_spec"))),
-                tabPanel("United States", plotOutput("us_scatter"), plotlyOutput("us_bar")))
-  )
+  titlePanel(strong("Mental Health in Tech")),  # Project Title bolded
+  mainPanel(
+      tabsetPanel(tabPanel("World Wide Info", br(), h4("World Wide Trends Plots"), plotlyOutput("scatter"), br(),
+                           p("The above scatterplot shows the average age of respondents in each
+                             country as well as whether the respondents have seeked help in the past.
+                             In the survey, individuals sometimes put 'Not sure' and that was 
+                             interpretted as no help seeked. The size of the dot depends on the number of respondents
+                             that responded in that certain way of that country."), br(),
+                           plotlyOutput("bar"), br(), p("The bar graph shows in detail the TOTAL number of 
+                                                        respondents who have not seeked help and have. For a 
+                                                        better breakdown the respondents who said 'Not sure' are
+                                                        shown as well."), br(),
+                           tableOutput("table"), br(), p("The table shows a breakdown on where all the data came
+                                                         from.")),
+                  tabPanel("United States", br(), plotlyOutput("us_scatter"), br(), br(), 
+                           plotlyOutput("us_bar")),
+                  tabPanel("Countries", br(), h4("Specific Country Age v. Company Offering Care Options"), 
+                            fluidRow(p("Select a country of interest (the first four countries have more documented 
+                                        data than others - the United States, United Kingdom, Canada, and Germany consist 
+                                         a majority of the data)"), 
+                                     selectInput("place", "Country of Interest",
+                                                  c("United States", "United Kingdom", "Canada", "Germany", "Australia", "Austria", 
+                                                    "Bahamas", "Belgium", "Bosnia and Herzegovina", "Brazil", "Bulgaria", "China", 
+                                                    "Colombia", "Costa Rica", "Croatia", "Czech Republic", "Denmark", "Finland", 
+                                                    "France", "Georgia", "Greece", "Hungary", "India", "Ireland", "Israel", "Italy", 
+                                                    "Japan", "Latvia", "Mexico", "Moldova", "Netherlands", "New Zealand", "Nigeria", 
+                                                    "Norway", "Philippines", "Poland", "Portugal", "Romania", "Russia", "Singapore", 
+                                                    "Slovenia", "South Africa", "Spain", "Sweden", "Switzerland", "Thailand", "Uruguay")), 
+                                                    plotlyOutput("scatter_spec")))
+                   
+      )
+    )
 )
+           
+    
