2322b748215ea7a462a9f5af656f126b6d8d95bb
Nicole Kuhn <nskuhn03@gmail.com>
2018-03-07 13:52:36 -0800

    NK latest UI

--- app.R
+++ app.R
@@ -2,11 +2,16 @@ library("dplyr")
 library("ggplot2")
 library("shiny")
 library("tidyr")
-library(DT)
+library("DT")
+
+tech.data <- read.csv('data/survey.csv', stringsAsFactors = FALSE) 
+tech.df <- data.frame(tech.data)
+View(tech.df)
+
 
 
 # Define UI for application that draws a histogram
-ui <- navbarPage("Mental Health in Tech",
+ui <- navbarPage(strong("Mental Health in Tech"),
   tabPanel("Introduction",
         wellPanel(
         h4(strong("Overview")),
@@ -53,13 +58,14 @@ ui <- navbarPage("Mental Health in Tech",
                # Left column - widgets
                
                # Widget 1: Radio buttons for choosing country
-               helpText(em("Choose a country to view its results")),  # Help text italicized
+               helpText(em("Choose a Country to Compare")),  # Help text italicized
                radioButtons("countryInput", "Country",
                             choices = c("USA","United Kingdom","Canada","Germany")
                ),
-            
+               br(),
+               br(),
                # Widget 2: Drop-down for choosing one of all possible responses
-               helpText(em("View Totals for each Survey Response")),  # Help text italicized
+               helpText(em("Choose a Response to Compare")),  # Help text italicized
                selectInput(
                  "responseInput", 
                  "Response Data",
@@ -69,14 +75,23 @@ ui <- navbarPage("Mental Health in Tech",
             mainPanel(  
               tabsetPanel(
                 tabPanel(
-                  "Table", 
-                  h4("Table of Responses - All Countries vs. Chosen Country"),
+                  "Table",
+                  br(),
+                  br(),
+                  h4("Data Table Analysis"),
+                  h2("Selected Country vs. All Countries"),
                   tableOutput("table1")
                 ),
                 tabPanel(
                   "Plot",
-                  h4("Selected Response for Chosen Country"),
-                  plotOutput("plot1")
+                  br(),
+                  h4("Bar Graph - Selected Country vs. All Countries"),
+                  h2("Selected Country"),
+                  plotOutput("plot1"),
+                  br(),
+                  h2("All Countries"),
+                  plotOutput("plot2")
+                  
                 )
               )
             )
@@ -121,7 +136,9 @@ ui <- navbarPage("Mental Health in Tech",
                      h4(strong("Team Information")), # bold section title
                       br(),
                       p("Class: INFO201"),
+                      br(),
                       p("Quarter: Winter 2018"),
+                      br(),
                       p("Team Members: Nicole Kuhn, Lisa Qing")
                      )
             )
@@ -129,11 +146,95 @@ ui <- navbarPage("Mental Health in Tech",
 )
 
 
-# Define server logic required to draw a histogram
+
 server <- function(input, output) {
-   
+
+# total <- length(tech.df$leave)
+# leave.very.easy <- filter(tech.df, leave == "Very easy"
+# leave.somewhat.easy <- filter(tech.df, leave == "Somewhat easy"
+# leave.somewhat.difficult <- filter(tech.df, leave == "Somewhat difficult"
+# leave.very.difficult <- filter(tech.df, leave == "Very difficult")
+# leave.dont.know <- filter(tech.df, leave == "Don't know")
+# leave.rows <- c("Very Easy", "Somewhat Easy", "Somewhat Difficult", "Very Difficult", "Don't Know")
+# leave.all.totals <- c(length(leave.very.easy$leave), length(leave.somewhat.easy$leave),
+#     length(leave.somewhat.difficult$leave), length(leave.very.difficult$leave),
+#     length(leave.dont.know$leave))  #total for each response
+# percentage.all <- leave.all.totals / total *100  #percentage of all responses
+# percentage.all.rounded <- round(percentage, digits = 1)
+# 
+# leave.df <- data.frame(Response = leave.rows, # Row names = each response
+#   Total_All_Countries = leave.all.totals,  # all countries - totals
+#   Percentage_All_Countries = percentage.all,  # all countries - percentage
+#   Total_Country = leave.country.all.totals,  # selected country - totals
+#   Percentage_Country = percentage.country)  # selected country - percentage
+# View(leave.df)
+
   
-}
+# Selected Country Responses
+  
+country.answer <- reactive ({
+  tech.df %>%
+  filter(Country == input$countryInput) %>%
+  filter(leave == input$responseInput)%>%
+  length()
+  return(country.answer)
+})     
+
+country.count <- reactive ({
+  tech.df %>%
+  filter(Country == input$countryInput) %>%
+  length()
+  return(country.count)
+})
+  
+country.percent <- (country.answer() / country.count())*100 %>%
+  round(country.percent, digits = 1)
+View(country.percent)
+
+
+# All Countries Responses 
+
+all.countries.answer <- reactive ({
+  tech.df %>%
+  filter(leave == input$responseInput)%>%
+  length()
+  return(all.countries.answer)  
+})
+
+all.countries.count <- length(tech.df, Country)
+
+all.countries.percent <- (all.countries.answer()/all.countries.count * 100) %>%
+  round(all.countries.percent, digits = 1)
+View(all.countries.percent)
+
+# New Dataframe
+leave.df <- data.frame(Country_total = country.answer(), Country_percent = country.percent,
+                       All_total = all.countries.answer(), All_percent = all.countries.percent)
+
+output$table1 <- renderTable({
+  ## parameter?  leave.df
+})
+  
+# Bar Chart
+  
+
+output$plot1 <- renderPlot ({
+  ggplot(data = )
+  
+})
+
+  # output$phonePlot <- renderPlot({
+  #   
+  #   # Render a barplot
+  #   barplot(WorldPhones[,input$region]*1000, 
+  #           main=input$region,
+  #           ylab="Number of Telephones",
+  #           xlab="Year")
+  # })
+
+})
+  
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
