28ea8827cd0ba1d6294bf8bf117634cd5bfbf7a7
Lisa Qing <lisaqing98@gmail.com>
2018-03-06 21:36:20 -0800

    Added all analysis to my tab - pushing directly to master as merge has issues (Lisa)

--- server.R
+++ server.R
@@ -3,18 +3,33 @@ library("shiny")
 library("ggplot2")
 library("dplyr")
 
-source("ReadingKagg.R")
-df <- getFile()
+# If opening zip
+# source("ReadingKagg.R")
+df <- read.csv("survey.csv", stringsAsFactors = FALSE)
 
 my.server <- function(input, output){
   
-  output$scatter <- renderPlotly({
+  worldWideFilter <- function(){
     data <- select(df, Country, Age, seek_help, care_options) %>% 
       filter(Age < 100, Age > 0) ## Filter out outliers such as 99999 years old
     data$Country[data$Country == "Bahamas, The"] = "Bahamas"
     
+    return(data)
+  }
+  
+  
+  output$scatterWorld <- renderTable({
+    if(input$checkbox1 == TRUE){
+      data <- worldWideFilter()
+      data$seek_help[data$seek_help == "Don't know"] = "No" 
+      data <- group_by(data, Country, seek_help) %>%
+        summarize(average_age = mean(Age), count = n())
+    }
+  })
+  
+  output$scatter <- renderPlotly({
+    data <- worldWideFilter()
     data$seek_help[data$seek_help == "Don't know"] = "No" 
-    
     data <- group_by(data, Country, seek_help) %>%
       summarize(average_age = mean(Age), count = n())
     
@@ -27,10 +42,48 @@ my.server <- function(input, output){
     
   })
   
-  output$bar <- renderPlotly({
-    survey.data <- select(df, Country, Age, seek_help, care_options) %>% 
-      filter(Age < 100, Age > 0) ## Filter out outliers such as 99999 years old
-    survey.data$Country[survey.data$Country == "Bahamas, The"] = "Bahamas"
+  output$barWorld<- renderTable({
+    if(input$checkbox2 == TRUE){
+      survey.data <- worldWideFilter()
+      survey.data.1 <- group_by(survey.data, seek_help, care_options) %>%
+        summarize(count = n())
+    }
+  })
+  
+  output$scatterUS <- renderTable({
+    if(input$checkbox3){
+      data <- select(df, Country, state, state_full, Age, seek_help, care_options) %>% 
+        filter(Age < 100, Age > 0, Country == "United States") 
+      ## Filter out outliers such as 99999 years old
+      data$seek_help[data$seek_help == "Don't know"] = "No" 
+      
+      data <- group_by(data, state, state_full, seek_help) %>%
+        summarize(average_age = mean(Age), count = n())
+    }
+  })
+  
+  output$us_scatter <- renderPlotly({
+    data <- select(df, Country, state, state_full, Age, seek_help, care_options) %>% 
+      filter(Age < 100, Age > 0, Country == "United States") 
+    ## Filter out outliers such as 99999 years old
+    data$seek_help[data$seek_help == "Don't know"] = "No" 
+    
+    data <- group_by(data, state, state_full, seek_help) %>%
+      summarize(average_age = mean(Age), count = n())
+    
+    
+    plot_ly(data = data, x = ~average_age, y = ~state, color = ~seek_help, size = ~count, colors = "Set1", type = 'scatter', 
+            text = ~paste(state_full, paste0("Average Age: ", round(average_age, digits = 1), 
+                                        paste0("\nNumber of respondents: ", count)), sep = "<br />"), hoverinfo = "text") %>%
+      layout(title = 'United States Detailed View of Age and Seeking Mental Health Help Trend \n(hover for more information) \n',
+             xaxis = list(title = 'Average Age'),
+             yaxis = list(title = 'State (abbreviated)')) 
+    
+    
+  })
+  
+  filterUSBar <- function(){
+    survey.data <- worldWideFilter()
     
     survey.data.1 <- group_by(survey.data, seek_help, care_options) %>%
       summarize(count = n())
@@ -43,6 +96,17 @@ my.server <- function(input, output){
     survey.data.2$No_help <- value1
     survey.data.2$Not_sure <- value2
     survey.data.2$Yes_help <- value3
+    return(survey.data.2)
+  }
+  
+  output$barUS <- renderTable({
+    if(input$checkbox4 == TRUE){
+      data <- filterUSBar();
+    }
+  })
+  
+  output$bar <- renderPlotly({
+    survey.data.2 <- filterUSBar();
     
     plot_ly(survey.data.2, x = ~care_options, y = ~No_help, type = 'bar', name = 'No help seeked') %>%
       add_trace(y = ~Not_sure, name = 'Not sure') %>%
@@ -50,56 +114,6 @@ my.server <- function(input, output){
       layout(yaxis = list(title = 'Count'), xaxis = list(title = 'Company Offered Care Options'),
              title = 'World Wide View of Seeking Mental Health Help and Offered Care Options', 
              barmode = 'group') 
-    
-    
-  })
-  
-  output$table <- renderTable({
-    survey.data <- select(df, Country, Age, seek_help, care_options) %>% 
-      filter(Age < 100, Age > 0) ## Filter out outliers such as 99999 years old
-    survey.data$Country[survey.data$Country == "Bahamas, The"] = "Bahamas"
-    
-    survey.data.1 <- group_by(survey.data, seek_help, care_options) %>%
-      summarize(count = n())
-    
-  })
-  
-  filter_data <- reactive({
-    data <- select(df, Country, Age, seek_help, care_options) %>% 
-      filter(Age < 100, Age > 0) ## Filter out outliers such as 99999 years old
-    data$Country[data$Country == "Bahamas, The"] = "Bahamas"
-    data$seek_help[data$seek_help == "Don't know"] = "No" 
-    data <- filter(data, Country == input$place)
-    data <- group_by(data, seek_help, care_options, Age) %>%
-      summarize(count = n())
-  })
-  
-  output$scatter_spec <- renderPlotly({
-    data1 <- filter_data()
-    
-    plot_ly(data = data1, x = ~Age, y = ~care_options, color = ~seek_help, size = ~count, colors = "Set1", type = 'scatter', 
-            text = ~paste(paste0("Age: ", Age), paste0("\nNumber of respondents: ", count), paste0("\nHelp Seeked: ", seek_help), 
-                          sep = "<br />"), hoverinfo = "text") %>%
-      layout(title = paste('Age v. Offered Care Options \n(w/ influence of company offered care options)\n'),
-             yaxis = list(title = 'Company Offered Care Options'), margin=list(l = 100), yaxis=list(tickprefix="\t"))
-  })
-  
-  output$us_scatter <- renderPlotly({
-    data <- select(df, Country, state, Age, seek_help, care_options) %>% 
-      filter(Age < 100, Age > 0, Country == "United States") 
-    ## Filter out outliers such as 99999 years old
-    data$seek_help[data$seek_help == "Don't know"] = "No" 
-    
-    data <- group_by(data, state, seek_help) %>%
-      summarize(average_age = mean(Age), count = n())
-    #data <- filter(data, Country == input$place)
-    
-    plot_ly(data = data, x = ~average_age, y = ~state, color = ~seek_help, size = ~count, colors = "Set1", type = 'scatter', 
-            text = ~paste(state, paste0("Average Age: ", round(average_age, digits = 1), 
-                                        paste0("\nNumber of respondents: ", count)), sep = "<br />"), hoverinfo = "text") %>%
-      layout(title = 'United States Detailed View of Age and Seeking Mental Health Help Trend \n(hover for more information) \n',
-             xaxis = list(title = 'Average Age'),
-             yaxis = list(title = 'State (abbreviated)')) 
     
     
   })
@@ -128,4 +142,24 @@ my.server <- function(input, output){
              xaxis = list(title = 'Company Offered Care Options'),
              yaxis = list(title = 'Count'), width = 800, height = 500)
   })
+  
+  
+  filter_data <- reactive({
+    data <- worldWideFilter()
+    data <- filter(data, Country == input$place)
+    data <- group_by(data, seek_help, care_options, Age) %>%
+      summarize(count = n())
+  })
+  
+  output$scatter_spec <- renderPlotly({
+    data1 <- filter_data()
+    
+    plot_ly(data = data1, x = ~Age, y = ~care_options, color = ~seek_help, size = ~count, colors = "Set1", type = 'scatter', 
+            text = ~paste(paste0("Age: ", Age), paste0("\nNumber of respondents: ", count), paste0("\nHelp Seeked: ", seek_help), 
+                          sep = "<br />"), hoverinfo = "text") %>%
+      layout(title = paste('Age v. Offered Care Options \n(w/ influence of company offered care options)\n'),
+             yaxis = list(title = 'Company Offered Care Options'), margin=list(l = 100), yaxis=list(tickprefix="\t"))
+  })
+  
+  
 }
--- ui.R
+++ ui.R
@@ -38,12 +38,17 @@ ui <- navbarPage("Mental Health in Tech",
         h4(strong("Questions")),
         p("We focused our analysis of this dataset on the following questions:"),
         br(),
-        p(em("How easy is it for you to take medical leave for a mental health condition?")),
+        h5(em("How easy is it for you to take medical leave for a mental health condition?")),
         p("Analysis for all countries vs. individual countries with the highest response rate (U.S., United Kingdom, Canada, and Germany)"),
         # (info relating to importance of medical leave for mental health)
         br(),
-        p(em("Lisa's question")),
-        p("description of question")
+        h5(em("Is there a correlation between those who seek mental health and their age? And how do company care options play a role?")),
+        p("The analysis focuses on all the data collected in all the countries in which the survey was conducted. However, as a majority of
+          the data came from the United States, another focus of the question is on the trend it has within the states. The data used to 
+          answer this question primarily focused on the survey questions:"),
+        p("Does your employer provide resources to learn more about mental health issues and how to seek help?/Have you seeked help in the past?"),
+        p("Do you know the options for mental health care your employer provides?"),
+        p("Demographic questions on age and location were also used.")
   )
   ),
   tabPanel("Medical Leave",  
@@ -83,42 +88,89 @@ ui <- navbarPage("Mental Health in Tech",
           )
            
     ),
-    tabPanel("Lisas", sidebarLayout(  
+    tabPanel("Seeking Help", sidebarLayout(  
       sidebarPanel(   
-        
-        # Left column - widgets
-        
-        # Widget 1: Radio buttons for choosing country
-        helpText(em("Choose a country to view its results")),  # Help text italicized
-        radioButtons("countryInput", "Country",
-                     choices = c("USA","United Kingdom","Canada","Germany")
-        ),
-        
-        # Widget 2: Drop-down for choosing one of all possible responses
-        helpText(em("View Totals for each Survey Response")),  # Help text italicized
-        selectInput(
-          "responseInput", 
-          "Response Data",
-          choices = c("Very Easy", "Somewhat Easy","Somewhat Difficult","Very Difficult","Don't Know")
-        )            
+        h2("Trends in Seeking Help with Age and Company Offered Care Options"), 
+        br(),
+        p("The interactive graphs to the right depict the the relationship between the age of the respondents and their past
+          history of seeking help as well as what influence clearly iterated company offered care options have has. A majority of the 
+          data is from the United States as it was most widely distributed there. So a bulk of the analysis made is skewed 
+          towards the results founded in the US. The first tab creates a general overview of what data was collected. 
+          The second tab will illustrate the results specifically in the US by the state in which the respondent is from. 
+          The third tab focuses on each individual country (upon selection)."), 
+        br(),
+        h3("Question: "),
+        p(em("Is there a correlation between those who seek mental health and their age? And how do company 
+          care options play a role?")),
+        br(),
+        h4("General Conclusion:"),
+        p("As stated earlier, the survey was not distributed evenly across locations, so the results may not be representative in
+          the end; however from the data gathered, these are the results found."),
+        br(),
+        p("From the data, generally, age does has little influence on seeking help.However, in some countries, the US speicifically, 
+          there seems to be a direct relationship as the younger the employee is the less likely they will seek help. Looking at 
+          all the countries, a majority of respondents replied with they had never seeked help for mental health in the past (for 
+          the purpose of analysis, the individuals that answered not sure were placed under the category of no help seeked). Diving 
+          deeper, the relationship between companies offering health care options were compared to individuals seeking health. From 
+          this there is a more obvious correlation - though a majority of respondents have no seeked mental health help, there is an
+          increase in those who do when their employer offers health care.")
       ), 
       mainPanel(
-        tabsetPanel(tabPanel("World Wide Info", br(), h4("World Wide Trends Plots"), plotlyOutput("scatter"), br(),
+        tabsetPanel(tabPanel("World Wide Info", 
+                             br(), 
+                             h3("World Wide Trend Plots"), 
+                             plotlyOutput("scatter"), 
+                             br(),
+                             fluidRow(checkboxInput("checkbox1", label = "Display Scatter Plot Data", value = FALSE)),
                              p("The above scatterplot shows the average age of respondents in each
                                country as well as whether the respondents have seeked help in the past.
                                In the survey, individuals sometimes put 'Not sure' and that was 
                                interpretted as no help seeked. The size of the dot depends on the number of respondents
-                               that responded in that certain way of that country."), br(),
-                             plotlyOutput("bar"), br(), p("The bar graph shows in detail the TOTAL number of 
-                                                          respondents who have not seeked help and have. For a 
-                                                          better breakdown the respondents who said 'Not sure' are
-                                                          shown as well."), br(),
-                             tableOutput("table"), br(), p("The table shows a breakdown on where all the data came
-                                                           from.")),
-                    tabPanel("United States", br(), plotlyOutput("us_scatter"), br(), br(), 
-                             plotlyOutput("us_bar")),
-                    tabPanel("Countries", br(), h4("Specific Country Age v. Company Offering Care Options"), 
-                             fluidRow(p("Select a country of interest (the first four countries have more documented 
+                               that responded in that certain way of that country. As seen in the data, the United States
+                               had the most number of respondents and of this, 561 people said no and 187 people said yes
+                               to have had seeked help/employers have informed them of how to. The other countries did not 
+                               have as many data points; however, it can still be seen that many respondents replied with
+                               no to the question of seeking help. From this scatter plot, It is not entirely clear if age
+                               has a correlation with seeking help, but that could potentially be due little collected data
+                               in each specific country (unlike in the US data where there is a clear relationship)"), 
+                             br(),
+                             tableOutput("scatterWorld"),
+                             plotlyOutput("bar"), 
+                             br(), 
+                             fluidRow(checkboxInput("checkbox2", label = "Display Bar Graph Data", value = FALSE)), 
+                             p("The bar graph shows in detail the TOTAL number of respondents who have not seeked help and have. 
+                                For a better breakdown the respondents, those who replied 'Not sure' are shown as well. From 
+                               the bar graph, it can be seen that when health care options are clearly iterated to the employees,
+                               the number of those who have seeked health increased significantly (167 respondents replied with yes)
+                               to those who were not or unsure. As seen in the scatterplot the US had the most number of 
+                               respondents to the survey, so this bar graph is in fact skewed towards the US respondents answers."), 
+                             br(),
+                             tableOutput("barWorld")),
+                    tabPanel("United States", 
+                             br(), 
+                             plotlyOutput("us_scatter"), 
+                             fluidRow(checkboxInput("checkbox3", label = "Display Scatter Plot Data", value = FALSE)),
+                             br(), 
+                             p("This scatter plot shows the correlation of age with seeking help. More older individuals responded
+                               to the survey with yes to seeking help. While more younger individuals responded to the survey with
+                               no/unsure of seeking help (like the first tab scatter plot those unsure of seeking help or have not
+                               were grouped in the same category for the purpose of analysis). The most respondents came from California,
+                               (95 replied no/unsure - ave age: 31.8 y.o. , and 43 replied yes - ave age: 36.1 y.o.). Many of the
+                               other states also show this similar result."),
+                             tableOutput("scatterUS"),
+                             plotlyOutput("us_bar"),
+                             br(), br(), br(), br(),
+                             fluidRow(checkboxInput("checkbox4", label = "Display Bar Graph Data", value = FALSE)),
+                             p(em("Side Note: same as the bar graph in the first tab, the data was separated (no's and unsure's) to
+                                  better recognize the relationships between the two responses")),
+                             p("As expected, those without clear information on if their respective companies had health care options,
+                               have not seeked help. However, when the respondents were clearly informed, those who have seeked help
+                               made a significant jump (from 33 (not informed) and 26 (unsure) to 128 (informed)."), 
+                             tableOutput("barUS")),
+                    tabPanel("Countries", 
+                             br(), 
+                             h4("Specific Country Age v. Company Offering Care Options"), 
+                             fluidRow(helpText("Select a country of interest (the first four countries have more documented 
                                         data than others - the United States, United Kingdom, Canada, and Germany consist 
                                         a majority of the data)"), 
                                       selectInput("place", "Country of Interest",
@@ -129,12 +181,20 @@ ui <- navbarPage("Mental Health in Tech",
                                                     "Japan", "Latvia", "Mexico", "Moldova", "Netherlands", "New Zealand", "Nigeria", 
                                                     "Norway", "Philippines", "Poland", "Portugal", "Romania", "Russia", "Singapore", 
                                                     "Slovenia", "South Africa", "Spain", "Sweden", "Switzerland", "Thailand", "Uruguay")), 
-                                      plotlyOutput("scatter_spec")))
-                    
-                             )
+                                      plotlyOutput("scatter_spec")),
+                             br(),
+                             p("The above plot shows the data on the selected country. In general, age only shows to have a relationship
+                               with seeking help in certain countries particularly those with more data points (i.e. the US, United Kingdom). 
+                               The United Kingdom also showed a similar trend to the US (tab 2) in younger individuals were less likely to seek help;
+                               however, the trend is not as obvious (may be due to less data). Germany actually showed a different result as 
+                               older individuals were less likely to to seek help even after being notified of care options. Many of the 
+                               other countries only had few surveys collected, so some trends are unrecognizable and most likely inaccurate
+                               of that country -- just something to keep in mind.")
+                    )
+        )
+        )
       )
-                         
-      )),  
+      ),  
     
     navbarMenu("Documentation",  # drop-down menu from main menu
              tabPanel("Dataset",
